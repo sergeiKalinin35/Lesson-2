30ca85844c2bd1919de44fe1cc4026c25bd6ec1f
Sergey <piolersa85@mail.ru>
2020-10-16 19:26:12 +0200

    Setup Launch Screen Storyboard

--- Lesson 2/ViewController.swift
+++ Lesson 2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redLabelValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
     }
+    
+    func changeColor() {
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value), alpha: 1)
+        
+    }
+    
+    @IBAction func redGreenBlueSliderChanged(_ sender: UISlider) {
+        
+        redLabelValue.text = String(format: "%.2f", redSlider.value )
+        greenLabelValue.text = String(format: "%.2f", greenSlider.value)
+        blueLabelValue.text = String(format: "%.2f", blueSlider.value)
+        
+        changeColor()
 
-
+    }
+    
 }
+
+
+
+
 
